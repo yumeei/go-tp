ddc1a24e436da749fb55e95b9e46d800700ca78b
Valérie Song <valerie.song@ormex.io>
2025-11-04 20:54:20 +0100

    feat: add get contact by ID + refactor: move in handlers

--- internal/app/app.go
+++ internal/app/app.go
@@ -12,7 +12,6 @@ import (
 
 func Run(store storage.Store) {
 	reader := bufio.NewReader(os.Stdin)
-	var contactList []*storage.Contact
 
 	for {
 		fmt.Println("Voici tous les menus disponibles:")
@@ -20,7 +19,8 @@ func Run(store storage.Store) {
 		fmt.Println("2- Lister tous les contacts")
 		fmt.Println("3- Supprimer un contact")
 		fmt.Println("4- Mettre à jour un contact")
-		fmt.Println("5- Quitter l'application")
+		fmt.Println("5- Chercher un contact par ID")
+		fmt.Println("6- Quitter l'application")
 		fmt.Print("Choisissez votre menu: ")
 
 		input, _ := reader.ReadString('\n')
@@ -28,91 +28,127 @@ func Run(store storage.Store) {
 
 		switch input {
 		case "1":
-			fmt.Println("-> Ajouter un contact")
-			fmt.Println("-> Rentrez le nom")
-			inputNom, _ := reader.ReadString('\n')
-			inputNom = strings.TrimSpace(inputNom)
-			fmt.Println("-> Rentrez le prenom")
-			inputPrenom, _ := reader.ReadString('\n')
-			inputPrenom = strings.TrimSpace(inputPrenom)
-			fmt.Println("-> Rentrez l'email")
-			inputMail, _ := reader.ReadString('\n')
-			inputMail = strings.TrimSpace(inputMail)
-
-			addedContact, err := storage.NewContact(inputNom, inputPrenom, inputMail)
-			if err != nil {
-				fmt.Printf("Erreur de validation: %v\n\n", err)
-				break
-			}
-			addedContactValue, err := store.AjouterContact(addedContact)
-			if err != nil {
-				fmt.Printf("Erreur d'ajout au manager: %v\n\n", err)
-			} else {
-				fmt.Printf("Utilisateur ajouté: %v \n\n", addedContactValue)
-			}
-
+			handleAddContact(reader, &store)
 		case "2":
-			fmt.Println("-> Lister tous les contacts")
-			contactList = store.GetContactsList()
-			if len(contactList) != 0 {
-				for i := 0; i < len(contactList); i++ {
-					fmt.Printf("Utilisateur n° %v : %v, %v, %v \n\n", i, contactList[i].Prenom, contactList[i].Nom, contactList[i].Email)
-				}
-			} else {
-				fmt.Println("Pas de contact enregistrés")
-			}
+			handleListContacts(store)
 		case "3":
-			fmt.Println("-> Supprimer un contact")
-			fmt.Println("-> Entrez l'ID du contact à supprimer :")
-			inputId, _ := reader.ReadString('\n')
-			inputId = strings.TrimSpace(inputId)
-			inputIdInt, err := strconv.ParseInt(inputId, 10, 64)
-			if err != nil {
-				fmt.Printf("Erreur dans la conversion de l'ID : %v", err)
-			} else {
-				err = store.SupprimerContact(uint(inputIdInt))
-				if err != nil {
-					fmt.Printf("Une erreur est survenue : %v\n\n", err)
-				} else {
-					fmt.Println("Contact supprimé avec succès")
-				}
-			}
+			handleDeleteContact(reader, &store)
 		case "4":
-			fmt.Println("-> Mettre à jour un contact")
-			fmt.Println("-> Rentrez l'ID")
-			inputId, _ := reader.ReadString('\n')
-			inputId = strings.TrimSpace(inputId)
-			inputIdInt, err := strconv.ParseInt(inputId, 10, 64)
-			if err != nil {
-				fmt.Printf("Erreur dans la conversion de l'ID %v", err)
-				break
-			}
-			fmt.Println("-> Rentrez le nouveau nom")
-			inputNom, _ := reader.ReadString('\n')
-			inputNom = strings.TrimSpace(inputNom)
-			fmt.Println("-> Rentrez le nouveau prenom")
-			inputPrenom, _ := reader.ReadString('\n')
-			inputPrenom = strings.TrimSpace(inputPrenom)
-			fmt.Println("-> Rentrez le nouvel email")
-			inputMail, _ := reader.ReadString('\n')
-			inputMail = strings.TrimSpace(inputMail)
-			updateContact, err := storage.NewContact(inputNom, inputPrenom, inputMail)
-			if err != nil {
-				fmt.Printf("Erreur à la création du contact: %v", err)
-				break
-			}
-			err = store.ModifierContact(uint(inputIdInt), updateContact)
-			if err != nil {
-				fmt.Printf("Une erreur est survenue: %v\n\n", err)
-			} else {
-				fmt.Printf("Utilisateur modifié \n\n")
-			}
+			handleUpdateContact(reader, &store)
 		case "5":
+			handleFindByID(reader, store)
+		case "6":
 			fmt.Println("-> Quitter l'application")
 			os.Exit(0)
 		default:
 			fmt.Println("Option invalide, essayez encore.")
 		}
 		fmt.Println()
+	}
+}
+
+func handleAddContact(reader *bufio.Reader, store *storage.Store) {
+	fmt.Println("-> Ajouter un contact")
+	fmt.Println("-> Rentrez le nom")
+	inputNom, _ := reader.ReadString('\n')
+	inputNom = strings.TrimSpace(inputNom)
+	fmt.Println("-> Rentrez le prenom")
+	inputPrenom, _ := reader.ReadString('\n')
+	inputPrenom = strings.TrimSpace(inputPrenom)
+	fmt.Println("-> Rentrez l'email")
+	inputMail, _ := reader.ReadString('\n')
+	inputMail = strings.TrimSpace(inputMail)
+
+	addedContact, err := storage.NewContact(inputNom, inputPrenom, inputMail)
+	if err != nil {
+		fmt.Printf("Erreur de validation: %v\n\n", err)
+		return
+	}
+	addedContactValue, err := store.AjouterContact(addedContact)
+	if err != nil {
+		fmt.Printf("Erreur d'ajout au manager: %v\n\n", err)
+	} else {
+		fmt.Printf("Utilisateur ajouté: %v \n\n", addedContactValue)
+	}
+}
+
+func handleListContacts(store storage.Store) {
+	fmt.Println("-> Lister tous les contacts")
+	contactList := store.GetContactsList()
+	if len(contactList) != 0 {
+		for i := 0; i < len(contactList); i++ {
+			fmt.Printf("Utilisateur n° %v : %v, %v, %v \n\n", i, contactList[i].Prenom, contactList[i].Nom, contactList[i].Email)
+		}
+	} else {
+		fmt.Println("Pas de contact enregistrés")
+	}
+}
+
+func handleDeleteContact(reader *bufio.Reader, store *storage.Store) {
+	fmt.Println("-> Supprimer un contact")
+	fmt.Println("-> Entrez l'ID du contact à supprimer :")
+	inputId, _ := reader.ReadString('\n')
+	inputId = strings.TrimSpace(inputId)
+	inputIdInt, err := strconv.ParseInt(inputId, 10, 64)
+	if err != nil {
+		fmt.Printf("Erreur dans la conversion de l'ID : %v", err)
+		return
+	}
+
+	err = store.SupprimerContact(uint(inputIdInt))
+	if err != nil {
+		fmt.Printf("Une erreur est survenue : %v\n\n", err)
+	} else {
+		fmt.Println("Contact supprimé avec succès")
+	}
+}
+
+func handleUpdateContact(reader *bufio.Reader, store *storage.Store) {
+	fmt.Println("-> Mettre à jour un contact")
+	fmt.Println("-> Rentrez l'ID")
+	inputId, _ := reader.ReadString('\n')
+	inputId = strings.TrimSpace(inputId)
+	inputIdInt, err := strconv.ParseInt(inputId, 10, 64)
+	if err != nil {
+		fmt.Printf("Erreur dans la conversion de l'ID %v", err)
+		return
+	}
+	fmt.Println("-> Rentrez le nouveau nom")
+	inputNom, _ := reader.ReadString('\n')
+	inputNom = strings.TrimSpace(inputNom)
+	fmt.Println("-> Rentrez le nouveau prenom")
+	inputPrenom, _ := reader.ReadString('\n')
+	inputPrenom = strings.TrimSpace(inputPrenom)
+	fmt.Println("-> Rentrez le nouvel email")
+	inputMail, _ := reader.ReadString('\n')
+	inputMail = strings.TrimSpace(inputMail)
+	updateContact, err := storage.NewContact(inputNom, inputPrenom, inputMail)
+	if err != nil {
+		fmt.Printf("Erreur à la création du contact: %v", err)
+		return
+	}
+	err = store.ModifierContact(uint(inputIdInt), updateContact)
+	if err != nil {
+		fmt.Printf("Une erreur est survenue: %v\n\n", err)
+	} else {
+		fmt.Printf("Utilisateur modifié \n\n")
+	}
+}
+
+func handleFindByID(reader *bufio.Reader, store storage.Store) {
+	fmt.Println("-> Chercher par ID")
+	fmt.Println("-> Rentrez l'ID")
+	inputId, _ := reader.ReadString('\n')
+	inputId = strings.TrimSpace(inputId)
+	inputIdInt, err := strconv.ParseInt(inputId, 10, 64)
+	if err != nil {
+		fmt.Printf("Erreur dans la conversion de l'ID %v", err)
+		return
+	}
+	contactFound, err := store.GetContactByID(uint(inputIdInt))
+	if err != nil {
+		fmt.Printf("Une erreur est survenue : %v", err)
+	} else {
+		fmt.Printf("Utilisateur trouvé : %v, %v, %v \n\n", contactFound.Prenom, contactFound.Nom, contactFound.Email)
 	}
 }
