bf6271d395c0a6e2a9b2fd1dc2cf057dc085b2fb
Ludovic MARIE <91457982+LudovicMARIE@users.noreply.github.com>
2025-12-06 18:23:09 +0100

    feat: update and list contact

--- cmd/crm/listContact.go
+++ cmd/crm/listContact.go
@@ -1,1 +1,31 @@
 package cmd_crm
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"github.com/yumeei/go-tp/internal/storage"
+)
+
+var listContactCmd = &cobra.Command{
+	Use:   "listContact",
+	Short: "list contact command",
+	Long:  `list contact command`,
+	Run: func(cmd *cobra.Command, args []string) {
+		var store = storage.NewMemoryStorage()
+
+		fmt.Println("-> Lister tous les contacts")
+		contactList := store.GetContactsList()
+		if len(contactList) != 0 {
+			for i := 0; i < len(contactList); i++ {
+				fmt.Printf("Utilisateur n° %v : %v, %v, %v \n\n", i, contactList[i].Prenom, contactList[i].Nom, contactList[i].Email)
+			}
+		} else {
+			fmt.Println("Pas de contact enregistrés")
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(listContactCmd)
+}
--- cmd/crm/updateContact.go
+++ cmd/crm/updateContact.go
@@ -1,1 +1,95 @@
 package cmd_crm
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/spf13/cobra"
+	"github.com/yumeei/go-tp/internal/storage"
+)
+
+var (
+	inputUpdateId   string
+	updateLastName  string
+	updateFirstName string
+	updateMail      string
+)
+
+var updateContactCmd = &cobra.Command{
+	Use:   "updateContact",
+	Short: "update contact command",
+	Long:  `update contact command`,
+	Run: func(cmd *cobra.Command, args []string) {
+		reader := bufio.NewReader(os.Stdin)
+		var store = storage.NewMemoryStorage()
+
+		var inputIdInt int64
+		var err error
+
+		// ID
+		if inputUpdateId == "" {
+			fmt.Println("-> Mettre à jour un contact")
+			fmt.Println("-> Rentrez l'ID")
+			inputId, _ := reader.ReadString('\n')
+			inputId = strings.TrimSpace(inputId)
+			inputIdInt, err = strconv.ParseInt(inputId, 10, 64)
+			if err != nil {
+				fmt.Printf("Erreur dans la conversion de l'ID %v\n", err)
+				return
+			}
+		} else {
+			inputId := strings.TrimSpace(inputUpdateId)
+			inputIdInt, err = strconv.ParseInt(inputId, 10, 64)
+			if err != nil {
+				fmt.Printf("Erreur dans la conversion de l'ID %v\n", err)
+				return
+			}
+		}
+
+		// Nom
+		if updateLastName == "" {
+			fmt.Println("-> Rentrez le nouveau nom")
+			n, _ := reader.ReadString('\n')
+			updateLastName = strings.TrimSpace(n)
+		}
+
+		// Prenom
+		if updateFirstName == "" {
+			fmt.Println("-> Rentrez le nouveau prenom")
+			p, _ := reader.ReadString('\n')
+			updateFirstName = strings.TrimSpace(p)
+		}
+
+		// Email
+		if updateMail == "" {
+			fmt.Println("-> Rentrez le nouvel email")
+			e, _ := reader.ReadString('\n')
+			updateMail = strings.TrimSpace(e)
+		}
+
+		updateContact, err := storage.NewContact(updateLastName, updateFirstName, updateMail)
+		if err != nil {
+			fmt.Printf("Erreur à la création du contact: %v\n", err)
+			return
+		}
+
+		err = store.ModifierContact(uint(inputIdInt), updateContact)
+		if err != nil {
+			fmt.Printf("Une erreur est survenue: %v\n\n", err)
+		} else {
+			fmt.Printf("Utilisateur modifié \n\n")
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(updateContactCmd)
+
+	updateContactCmd.Flags().StringVarP(&inputUpdateId, "contactId", "c", "", "Id du contact à modifier")
+	updateContactCmd.Flags().StringVarP(&updateLastName, "lastName", "l", "", "Nouveau nom de famille du contact")
+	updateContactCmd.Flags().StringVarP(&updateFirstName, "firstName", "f", "", "Nouveau prénom du contact")
+	updateContactCmd.Flags().StringVarP(&updateMail, "mail", "m", "", "Nouveau mail du contact")
+}
